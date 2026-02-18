ae2cc1e1012895b401614ed21d6f5fcf617d411f
BJP103 <BJP103@pitt.edu>
2026-02-17 22:57:19 -0500

    Working on task button

--- state/src/component/taskcard.jsx
+++ state/src/component/taskcard.jsx
@@ -1,10 +1,21 @@
-
+import './task.css';
+import React, {useState} from 'react';
 export function Test({title, description}){
+
+    const done = () =>{
+        
+    }
+
     return(
-        <div>
+        <div id ='task' className='task'>
             <h1>{title}</h1>
             <p>{description}</p>
+            <button onClick={done}>Done</button>
         </div>
         
     )
 }
+
+const style = StyleSheet.create({
+    
+})
